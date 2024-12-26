42810daffd181876daa28faf3272cee24eda0d34
Sahil Garg <Sahil.Garg@evryindia.in>
2024-12-26 13:50:41 +0530

    appspec

--- scripts/start_apache.sh
+++ scripts/start_apache.sh
@@ -6,3 +6,7 @@ sudo systemctl start httpd
 
 # Check Apache service status
 sudo systemctl status httpd
+-
+#!/bin/bash
+cd /home/ec2-user/my-react-app
+npm run start  # Or use pm2 to run your React app in the background
--- scripts/start_server.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /var/www/reactapp
-npm run start  # Or use pm2 to run your React app in the background
