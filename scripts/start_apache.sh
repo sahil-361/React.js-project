861cfbff161aaf3bd9154c4a25c38b51547f4459
Sahil Garg <Sahil.Garg@evryindia.in>
2024-12-30 20:52:06 +0530

    files changed

--- scripts/start_apache.sh
+++ scripts/start_apache.sh
@@ -14,4 +14,7 @@ sudo systemctl status httpd
 #!/bin/bash
 cd /home/ec2-user/my-react-app
 
+npm install react-scripts --save
+
+
 npm run start  # Or use pm2 to run your React app in the background
